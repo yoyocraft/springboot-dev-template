3d3f9e0765d3de350cb1ee22b6127454ee1c736b
youyi <youyichannel@163.com>
2024-09-18 22:08:46 +0800

    修改依赖

--- src/main/java/com/youyi/example/MainController.java
+++ src/main/java/com/youyi/example/MainController.java
@@ -1,14 +1,14 @@
 package com.youyi.example;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 系统检查使用，请勿删除
  * @author yoyocraft
  * @date 2024/09/17
  */
-@Controller
+@RestController
 public class MainController {
 
     @GetMapping("/check")
